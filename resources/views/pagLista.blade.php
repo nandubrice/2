adcf80edb2793440edadb1c3edba39a62ea15ae6
Arnado Briceño Catacora <bricenoarnaldo7@gmail.com>
2022-12-07 17:54:56 -0500

    version05

--- resources/views/pagLista.blade.php
+++ resources/views/pagLista.blade.php
@@ -7,25 +7,27 @@
 
 @section('cuerpo')
     @if(session('msj'))
-      <div class="alert alert-sucess alert-dismissible fade show" role="alert">
+      <div class="alert alert-primary alert-dismissible fade show" role="alert">
         {{ session('msj') }}
         <button type="button" class="btn-close" data-bs-dismiss="alert" arial-label="close"></button>
       </div>
     @endif
-
+<div class="btn btn-danger d-grid fs-5 mb-2"> REGISTRAR NUEVO SEGUIMIENTO... </div>
     <form action="{{ route('Estudiante.xRegistrar') }}" method="post" class="d-grid gap-2">
       @csrf
-      @error('codEst')
-          <div class="alert alert-danger">
-            El codigo es requerido
+      @if($errors ->has('codEst'))
+          <div class="alert alert-warning alert-dismissible fade show" role="alert">
+            El <strong>apellido</strong> es requerido
+            <button type="button" class="btn-close" data-bs-dismiss="alert" arial-label="Close"></button>
           </div>
-      @enderror
+      @endif
 
-      @error('nomEst')
-          <div class="alert alert-danger">
-              El nombre es requerido
+      @if($errors ->has('nomEst'))
+          <div class="alert alert-warning alert-dismissible fade show" role="alert">
+            El <strong>apellido</strong> es requerido
+            <button type="button" class="btn-close" data-bs-dismiss="alert" arial-label="Close"></button>
           </div>
-      @enderror
+      @endif
 
       @if($errors ->has('apeEst'))
           <div class="alert alert-warning alert-dismissible fade show" role="alert">
@@ -34,6 +36,18 @@
           </div>
       @endif
 
+      @if($errors ->has('fnaEst'))
+          <div class="alert alert-warning alert-dismissible fade show" role="alert">
+            El <strong>FECHA</strong> es requerido
+            <button type="button" class="btn-close" data-bs-dismiss="alert" arial-label="Close"></button>
+          </div>
+      @endif
+      @if($errors ->has('turMat'))
+          <div class="alert alert-warning alert-dismissible fade show" role="alert">
+            El <strong>TURNO</strong> es requerido
+            <button type="button" class="btn-close" data-bs-dismiss="alert" arial-label="Close"></button>
+          </div>
+      @endif
       <input type="text" name="codEst" placeholder="Codigo" value="{{ old('codEst') }}" class="form-control mb-2">
       <input type="text" name="nomEst" placeholder="Nombres" value="{{ old('nomEst') }}" class="form-control mb-2">
       <input type="text" name="apeEst" placeholder="Apellidos" value="{{ old('apeEst') }}" class="form-control mb-2">
@@ -55,34 +69,9 @@
         <option value="0">Inactivo</option>
         <option value="1">Activo</option>
       </select>
-      <button class="btn btn-primary" type="submit">AGREGAR</button>
+      <button class="btn btn-danger" type="submit">AGREGAR</button>
 
     </form>
-    <h3>LISTA</h3>
-    <table class="table">
-        <thead class="table-dark">
-        <tr>
-            <th scope="col">ID</th>
-            <th scope="col">CODIGO</th>
-            <th scope="col">APELLIDOS Y NOMBRES</th>
-            <th scope="col">Handle</th>
-        </tr>
-  </thead>
-  <tbody>
-    @foreach($xAlumnos as $item)
-    <tr>
-      <th scope="row">{{ $item->id }}</th>
-      <td>{{ $item->codEst }}</td>
-      <td>
-        <a href="{{ route('Estudiante.xDetalle', $item->id ) }}">  
-             {{ $item->apeEst }}, {{ $item->nomEst }}
-        </a>
-      </td>
-        <td>@mdo</td>
-    </tr>
-    @endforeach
-  </tbody>
-</table>
 @endsection
 
 
