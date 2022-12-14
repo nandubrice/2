cb1c0f69e5642b82df64cfff4305a1f40216bce4
Arnado Briceño Catacora <bricenoarnaldo7@gmail.com>
2022-12-14 10:57:33 -0500

    actualizado

--- resources/views/pagLista.blade.php
+++ resources/views/pagLista.blade.php
@@ -72,6 +72,31 @@
       <button class="btn btn-danger" type="submit">AGREGAR</button>
 
     </form>
+    <h3>LISTA</h3>
+    <table class="table">
+        <thead class="table-dark">
+        <tr>
+            <th scope="col">ID</th>
+            <th scope="col">CODIGO</th>
+            <th scope="col">APELLIDOS Y NOMBRES</th>
+            <th scope="col">EDITAR</th>
+        </tr>
+  </thead>
+  <tbody>
+    @foreach($xAlumnos as $item)
+    <tr>
+      <th scope="row">{{ $item->id }}</th>
+      <td>{{ $item->codEst }}</td>
+      <td>
+        <a href="{{ route('Estudiante.xDetalle', $item->id ) }}">  
+             {{ $item->apeEst }}, {{ $item->nomEst }}
+        </a>
+      </td>
+        <td>@mdo</td>
+    </tr>
+    @endforeach
+  </tbody>
+</table>
 @endsection
 
 
